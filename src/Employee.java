1d93e2167efe94ebe1117fea669718d8ed268742
felixrichnow <felix.richnau@gmail.com>
2017-03-30 15:44:49 +0200

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 2017-03-30.
+ */
+public abstract class Employee {
+    int EmployeeID;
+    String name;
+    int gender;
+    int salary;
+
+}
+
---
+++ src/Management.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+/**
+ * Created by user on 2017-03-30.
+ */
+public class Management {
+    ArrayList<Designer> designerArayList;
+    ArrayList<Designer> adminArayList;
+    private int averageSalary;
+
+    public void removeEmployy(){
+        //TODO write this method
+    }
+
+    public void addEmployy(){
+        //TODO write this method
+    }
+
+    public void calculateAverageDesignSalary(){
+        //Goes through the whole EmployeeArrayList and calculates AverageSalary
+        //Store it in an int
+        for(int i=0 ; i<designerArayList.size(); i++){
+            //Once employee/professions are done this can be implemented
+        }
+        //divide total salary by amount of employees
+    }
+
+    public findOutBouns(){
+
+    }
+
+    public ArrayList<Designer> getAccessToDesigners(){
+        return designerArayList;
+    }
+
+    public ArrayList<Designer> getAccessToAdmins(){
+        return adminArayList;
+    }
+
+
+}
